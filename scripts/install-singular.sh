1054a2d1079bf2e4f289165103f7f5cd9398acb2
Ming-Hsien Tsai <mhtsai208@gmail.com>
2022-01-15 09:04:53 +0800

    Update scripts.

--- scripts/install-singular.sh
+++ scripts/install-singular.sh
@@ -6,7 +6,7 @@ SINGULAR_DIR=singular-${SINGULAR_VERSION}
 SINGULAR_TAR_GZ=singular-${SINGULAR_VERSION}.tar.gz
 SINGULAR_URL=ftp://jim.mathematik.uni-kl.de/pub/Math/Singular/SOURCES/${SINGULAR_VERSION//./-}/${SINGULAR_TAR_GZ}
 
-apt install -y curl build-essential autoconf autogen libtool \
+sudo apt install -y curl build-essential autoconf autogen libtool \
 	libreadline6-dev libglpk-dev libgmp-dev libmpfr-dev libcdd-dev \
 	libntl-dev libflint-dev 4ti2
 mkdir -p ${TOOLS_DIR}
@@ -16,7 +16,7 @@ tar zxf ${SINGULAR_TAR_GZ}
 pushd ${SINGULAR_DIR}
 ./configure --enable-gfanlib
 make
-make install
+sudo make install
 sudo ldconfig
 popd
 popd
