afc326e29af829433b0bd3727e6e6d3dbe248167
bywang <bowyaw@bitbucket.org>
2023-04-21 10:21:53 +0800

    enable avx512vlx2, avx512vlx8, avx512x16

--- examples/openssl/chacha20/top.c
+++ examples/openssl/chacha20/top.c
@@ -1,15 +1,21 @@
 #include <stdint.h>
 
-uint8_t out[128], inp[128];
+uint8_t out[2048], inp[2048];
 uint32_t key[8] = { 0, 1, 2, 3, 4, 5, 6, 7 };
 uint32_t counter[4] = { 0x00, 0x10, 0x20, 0x30 };
 
 int main (void)
 {
   int i;
-  for (i = 0; i < 128; i++)
+  OPENSSL_cpuid_setup ();
+  for (i = 0; i < 2048; i++)
     inp[i] = 0x42;
-  ChaCha20_ctr32 (out, inp, 20, key, counter);
+  /* ChaCha20_avx512vl x 2 */
+  /* ChaCha20_ctr32 (out, inp, 128, key, counter); */
+  /* ChaCha20_avx512vl x 8 */
+  /* ChaCha20_ctr32 (out, inp, 256, key, counter); */
+  /* ChaCha20_avx512   x 16 */
+  ChaCha20_ctr32 (out, inp, 1024, key, counter);
   return 0;
 }
 
