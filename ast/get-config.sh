91b7565a917ef8870a8b0de3756572fcf4284c00
Ming-Hsien Tsai <mhtsai208@gmail.com>
2024-07-11 09:12:36 +0100

    Use ocamlfind to query zarith version.

--- ast/get-config.sh
+++ ast/get-config.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-if [[ -f `ocamlc -where`/../zarith/META ]]; then
-  meta=`ocamlc -where`/../zarith/META
-elif [[ -f `ocamlc -where`/zarith/META ]]; then
-  meta=`ocamlc -where`/zarith/META
-fi
-
-version=$(cat ${meta} | grep -m1 "version" | cut -d "=" -f 2 | tr -d '"')
+version=`ocamlfind query -format %v zarith`
 major=$(echo $version | cut -d "." -f 1)
 minor=$(echo $version | cut -d "." -f 2)
 echo "[%%define Z_version (${major}, ${minor})]"
