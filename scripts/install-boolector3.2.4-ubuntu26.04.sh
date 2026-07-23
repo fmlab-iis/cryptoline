bd465357d1243d96283c25dac158fe055c225264
Ming-Hsien Tsai <mhtsai208@gmail.com>
2026-07-24 00:13:05 +0800

    Add scripts to install external tools on Ubuntu 26.04.

---
+++ scripts/install-boolector3.2.4-ubuntu26.04.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Tested on:
+# - Ubuntu: 26.04
+
+TOOLS_DIR=tools
+BOOLECTOR_VERSION=3.2.4
+BOOLECTOR_TAR_GZ=${BOOLECTOR_VERSION}.tar.gz
+BOOLECTOR_DIR=boolector-${BOOLECTOR_VERSION}
+BOOLECTOR_URL=https://github.com/Boolector/boolector/archive/refs/tags/${BOOLECTOR_VERSION}.tar.gz
+
+sudo apt install -y curl build-essential binutils cmake git
+mkdir -p ${TOOLS_DIR}
+pushd ${TOOLS_DIR}
+curl -L ${BOOLECTOR_URL} -o ${BOOLECTOR_TAR_GZ}
+tar xvf ${BOOLECTOR_TAR_GZ}
+pushd ${BOOLECTOR_DIR}
+./contrib/setup-cadical.sh
+CMAKE_POLICY_VERSION_MINIMUM=3.5 ./contrib/setup-btor2tools.sh
+CMAKE_POLICY_VERSION_MINIMUM=3.5 ./configure.sh
+cd build
+make
+sudo make install
+cd ..
+popd
+popd
