5ee397659d54f260afc2d1f3aeccf1bdc4d5d77e
Ming-Hsien Tsai <mhtsai208@gmail.com>
2026-07-28 21:44:47 +0800

    Update install-abc.sh.

--- scripts/install-abc.sh
+++ scripts/install-abc.sh
@@ -2,19 +2,14 @@
 
 TOOLS_DIR=tools
 ABC_NAME=abc
-ABC_VERSION=a9237f50ea01efdd62f86d334a38ffbe80a3d141
-ABC_FILE=${ABC_NAME}-${ABC_VERSION}.tar.gz
-ABC_DIR=${ABC_NAME}-${ABC_VERSION}
-ABC_URL=https://github.com/berkeley-abc/${ABC_NAME}/archive/${ABC_VERSION}.tar.gz
+ABC_URL=https://github.com/berkeley-abc/abc.git
 
 sudo apt install -y curl build-essential binutils cmake libreadline-dev
 mkdir -p ${TOOLS_DIR}
 pushd ${TOOLS_DIR}
-curl -L ${ABC_URL} -o ${ABC_FILE}
-tar xvf ${ABC_FILE}
-mv ${ABC_FILE/.tar.gz/} ${ABC_DIR}
+git clone ${ABC_URL} ${ABC_NAME}
 
-pushd ${ABC_DIR}
+pushd ${ABC_NAME}
 make
 sudo mkdir -p /usr/local/bin/
 sudo install -m 755 -s abc /usr/local/bin/
